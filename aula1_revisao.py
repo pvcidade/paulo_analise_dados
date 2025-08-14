07199df930d8d4331031e7924c5786b2ba7ce8db
PAULO VITOR CIDADE DE SA <pvcidade@gmail.com>
2025-08-14 09:02:35 -0300

    aula_2_pandas

--- aula1_revisao.py
+++ aula1_revisao.py
@@ -3,3 +3,7 @@ y = 0
 z = 1.1
 lista = [x, y, z]
 dicionario = {"nome": x}
+
+import pandas as pd
+pd.read_excel("C:\\Users\\202408652135\\Documents\\analisededados\\livros_classics.xlsx")
+df = pd.read_excel("C:\\Users\\202408652135\\Documents\\analisededados\\livros_classics.xlsx")
